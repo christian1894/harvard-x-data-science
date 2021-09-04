cb568060ae42c09dc3a828aea27925dad59d6fdf
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 <christianp1894@gmail.com>
2021-09-03 19:35:12 -0500

    continuous probability notes, act scores assesment

---
+++ probability/continuous-probability/continuous-probability.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(dslabs)
+data(heights)
+x <- heights %>% filter(sex=="Male") %>% pull(height)
+# cumulative distribution function
+F = function(value) {
+  mean(x <= value)
+}
+1 - F(70.5) # eCDF
+
+1 - pnorm(70.5, mean(x), sd(x)) # Theoretical probability distribution
+
+# plot distribution of exact heights in data
+plot(prop.table(table(x)), xlab = "a = Height in inches", ylab = "Pr(x = a)")
+
+# probabilities in actual data over length 1 ranges containing an integer
+mean(x <= 68.5) - mean(x <= 67.5)
+mean(x <= 69.5) - mean(x <= 68.5)
+mean(x <= 70.5) - mean(x <= 69.5)
+
+# probabilities in normal approximation match well
+pnorm(68.5, mean(x), sd(x)) - pnorm(67.5, mean(x), sd(x))
+pnorm(69.5, mean(x), sd(x)) - pnorm(68.5, mean(x), sd(x))
+pnorm(70.5, mean(x), sd(x)) - pnorm(69.5, mean(x), sd(x))
+
+# probabilities in actual data over other ranges don't match normal approx as
+# well due to discretization(discrete values tend to be more common than 
+# continuous ones since people usually round their height to the nearest inch)
+mean(x <= 70.9) - mean(x <= 70.1)
+pnorm(70.9, mean(x), sd(x)) - pnorm(70.1, mean(x), sd(x))
+
+# generate simulated height data using normal distribution -
+# both datasets should have n observations
+n <- length(x)
+avg <- mean(x)
+s <- sd(x)
+simulated_heights <- rnorm(n, avg, s)
+
+# plot distribution of simulated_heights
+data.frame(simulated_heights = simulated_heights) %>%
+  ggplot(aes(simulated_heights)) +
+  geom_histogram(color="black", binwidth = 2)
+B <- 10000
+tallest <- replicate(B, {
+  simulated_data <- rnorm(800, avg, s)    # generate 800 normally distributed random heights
+  max(simulated_data)    # determine the tallest height
+})
+mean(tallest >= 7*12)    # proportion of times that tallest person exceeded 7 feet (84 inches)
+
+# normal distribution plot
+x <- seq(-4, 4, length.out = 100)
+data.frame(x, f = dnorm(x)) %>%
+  ggplot(aes(x,f)) +
+  geom_line()
